41e38ccf260ce2871b76a0b48f9d506acb43f345
smartzcy <3026478978@qq.com>
2023-05-05 16:28:54 +0800

    2023_3_5_1

--- Algorithm_java/test1/src/Lesson8/DynamicBindingDemo.java
+++ Algorithm_java/test1/src/Lesson8/DynamicBindingDemo.java
@@ -2,10 +2,10 @@ package Lesson8;
 
 public class DynamicBindingDemo {
     public static void main(String[] args) {
-        m(new GraduateStudent());
-        m(new Student());
-        m(new Person());
-        m(new Object());
+        // m(new GraduateStudent());
+        // m(new Student());
+        // m(new Person());
+        // m(new Object());
     }
 
     public static void m(Object x) {
